d3bd31a8d7bec955b401af027faa434eca432c5c
Gabriel Henrique DEV <gabriell.henriq@hotmail.com>
2025-07-10 12:57:28 -0300

    Página Location iniciada (apenas a estrutura e estilização basico)

---
+++ src/pages/Locations.tsx
@@ -0,0 +1,11 @@
+import LocationHeader from "../components/LocationHeader";
+
+export default function Locations() {
+  return (
+      <div style={{ background: '#fff', minHeight: '100vh' }}>
+        <LocationHeader/>
+      </div>
+       )
+} 
+ 
+     
