2072d3489dc3f056c162948cfdab80583fd83c00
victor batista <victo@LAPTOP.com>
2025-09-24 17:25:50 -0300

    so para finalizar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 
-import Residential from './pages/Residential';
 import Multifamily from './pages/Multifamily';
 import Commercial from './pages/Commercial';
 import Services from './pages/Services';
-import Locations from './pages/Locations';
-import Reviews from './pages/Reviews';
 import InteriorPainting from './pages/InteriorPainting';
 import ExteriorPainting from './pages/ExteriorPainting';
 import Cabinets from './pages/Cabinets';
@@ -33,13 +30,10 @@ function App() {
       <main className='PagesContent'>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/residential" element={<Residential />} />
           <Route path="/hoa" element={<Multifamily />} />
           <Route path="/multifamily" element={<Multifamily />} />
           <Route path="/commercial" element={<Commercial />} />
           <Route path="/services" element={<Services />} />
-          <Route path="/locations" element={<Locations />} />
-          <Route path="/reviews" element={<Reviews />} />
           <Route path="/interior-painting" element={<InteriorPainting />} />
           <Route path="/exterior-painting" element={<ExteriorPainting />} />
           <Route path="/cabinets" element={<Cabinets />} />
